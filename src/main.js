552e5011da4491342ede41c3ec5f77f426794f08
glidelos <66771420+glidelos@users.noreply.github.com>
2022-07-06 21:43:25 +0200

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,3 @@
-import "primevue/resources/themes/lara-light-indigo/theme.css";
-import "primevue/resources/primevue.min.css";
-import "primeicons/primeicons.css";
-
 import { createApp } from "vue";
 import App from "./App.vue";
 import PrimeVue from "primevue/config";
